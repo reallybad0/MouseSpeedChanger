9c3399f689e206451969e42ea7ac7716bfbc6ff9
fiserkl15 <fiserkl15@sps-prosek.cz>
2018-06-14 16:49:02 +0200

    Version 2.0

--- Mouse/MainWindow.xaml.cs
+++ Mouse/MainWindow.xaml.cs
@@ -21,34 +21,58 @@ namespace Mouse
     /// </summary>
     public partial class MainWindow : Window
     {
-        public const UInt32 SPI_SETMOUSESPEED = 0x0071;
-        public const UInt32 SPI_GETMOUSESPEED = 0x0070;
-
+        //
         [DllImport("User32.dll")]
         public static extern int SystemParametersInfo(uint uiAction, uint uiParam, uint pvParam, uint fWinIni);
+    
+        //
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool SystemParametersInfo(uint uiAction, uint uiParam, ref uint pvParam, uint fWinIni);
+
+        private const uint SPI_GETMOUSESPEED = 0x0070;
         public MainWindow()
         {
 
             InitializeComponent();
+            //Progress bar setup
+            
             PB.Maximum = 20;
-            PB.Value = 10;
+
+            //Current mouse sensitivity
+            uint currentMouseSensitivity = getCurrentMouseSensitivity();
+            PB.Value = currentMouseSensitivity;
+
             PB.Minimum = 1;
+
+
+            //Label setup            
             SP.Content = PB.Value.ToString();
-            //Set default = int res = SystemParametersInfo(113, 0, 10, 0);
+
         }
 
         private void SpeedBTN_Click(object sender, RoutedEventArgs e)
         {
-
+            //Changes speed to default
             int res = SystemParametersInfo(113, 0, 10, 0);
             PB.Value = 10;
         }
 
         private void PB_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
+            //Change the speed when progress bar moves
             UInt32 uservalue = (UInt32)PB.Value;
             SP.Content = uservalue.ToString();
+
             int res = SystemParametersInfo(113, 0, uservalue, 0);
+        }
+        private static uint getCurrentMouseSensitivity()
+        {
+            
+            uint result = 0;
+            //Get the mouse speed
+            SystemParametersInfo(SPI_GETMOUSESPEED, 0x0, ref result, 0x0);
+            return result;
+
         }
     } 
 }
